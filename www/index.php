2704183560c7e937dc660b715342ca25ba4ff60c
Eric Neves <ericnevesrodrigues@gmail.com>
2023-11-04 12:08:15 -0400

    Refactor: Remove content-type from index

--- www/index.php
+++ www/index.php
@@ -26,7 +26,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     header("Access-Control-Allow-Credentials: true");
-    header('Content-type: application/json');
     http_response_code(200); 
     exit;
   }
@@ -35,7 +34,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
   header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   header("Access-Control-Allow-Credentials: true");
-  header('Content-type: application/json');
 
 
 /**
