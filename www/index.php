b06dfeb76ca3801232f7ff0237b870af65de2b0b
Eric Neves <ericnevesrodrigues@gmail.com>
2023-11-04 12:34:24 -0400

    Feat: change cors config

--- www/index.php
+++ www/index.php
@@ -24,17 +24,14 @@ error_reporting(0);
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Headers: Content-Type");
     http_response_code(200); 
     return;
   }
   
   header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
-  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  header("Access-Control-Allow-Credentials: true");
-
+  header("Access-Control-Allow-Headers: Content-Type");
 
 /**
  * 
